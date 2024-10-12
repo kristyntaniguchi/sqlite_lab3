d36b40d9198f15bfe1c7ef8288d1d8971878d832
Kristyn Taniguchi <90283966+kristyntaniguchi@users.noreply.github.com>
2024-10-12 15:00:58 -0700

    Finished Delete

--- app/src/main/java/com/example/sqlite_lab3/UpdateCourseActivity.java
+++ app/src/main/java/com/example/sqlite_lab3/UpdateCourseActivity.java
@@ -13,7 +13,7 @@ public class UpdateCourseActivity extends AppCompatActivity {
 
     // variables for our edit text, button, strings and dbhandler class.
     private EditText courseNameEdt, courseTracksEdt, courseDurationEdt, courseDescriptionEdt;
-    private Button updateCourseBtn;
+    private Button updateCourseBtn, deleteCourseBtn;
     private DBHandler dbHandler;
     String courseName, courseDesc, courseDuration, courseTracks;
 
@@ -28,6 +28,7 @@ public class UpdateCourseActivity extends AppCompatActivity {
         courseDurationEdt = findViewById(R.id.idEdtCourseDuration);
         courseDescriptionEdt = findViewById(R.id.idEdtCourseDescription);
         updateCourseBtn = findViewById(R.id.idBtnUpdateCourse);
+        deleteCourseBtn = findViewById(R.id.idBtnDelete);
 
         // on below line we are initializing our dbhandler class.
         dbHandler = new DBHandler(UpdateCourseActivity.this);
@@ -63,5 +64,18 @@ public class UpdateCourseActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
-    }
-}
+
+        // adding on click listener for delete button to delete our course.
+        deleteCourseBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // calling a method to delete our course.
+                dbHandler.deleteCourse(courseName);
+                Toast.makeText(UpdateCourseActivity.this, "Deleted the course", Toast.LENGTH_SHORT).show();
+                Intent i = new Intent(UpdateCourseActivity.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
+
+    }//end of onCreate
+}//end of UpdateCourseActivity class
